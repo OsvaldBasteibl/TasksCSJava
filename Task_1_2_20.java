f3ce380f893a335b48744758b4f24ede93d800b5
OsvaldBasteibl <85823775+OsvaldBasteibl@users.noreply.github.com>
2022-01-15 00:37:00 +0300

    Задача №1.2.20

---
+++ Task_1_2_20.java
@@ -0,0 +1,12 @@
+public class Task_1_2_20 {
+    public static void main(String[] args) {
+        int a, b;
+        a = (int)(Math.random() * 6) + 1;
+        b = (int)(Math.random() * 6) + 1;
+        System.out.println("a = " + a + " b = " + b);
+        System.out.println("a + b = " + (a + b));
+
+    }
+}
+//1.2.20.Напишите программу, которая выводит сумму двух случайных чисел
+//от 1 до 6 (такие результаты могут быть получены при броске кубиков).
